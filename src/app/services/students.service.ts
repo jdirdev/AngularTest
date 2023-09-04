850bbd933a439a83224d689c40c15d0c8a4ff274
jdirdev <jodiegoro@hotmail.com>
2023-09-04 12:10:59 -0500

    Método Innecesario de Pruebas

--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -11,18 +11,6 @@ export class StudentsService {
     constructor(private httpClient:HttpClient) {
     }
 
-    getPersons(): Array<any> {
-
-      let persons: Array<any> = [];
-      for(let index=0; index<20; index++ ) {
-        persons.push({name: "Nombre", lastname: "Apellido", documenttype:"cc", document:"8888", country:"Bogota"});
-      }
-      for(let index=0; index<30; index++ ) {
-        persons.push({name: "Ted", lastname: "Test", documenttype:"cc", document:"8888", country:"Bogota"});
-      }
-      return persons;
-    }
-
     getStudents(): Observable<Array<Student>> {
         const url: string = "http://localhost:9000/mydrupal/API/formulario";
         return this.httpClient.get<Array<Student>>(url);
